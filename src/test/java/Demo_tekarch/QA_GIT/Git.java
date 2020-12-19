4ae5a02349e5bc9de183bf7ca0ab68126805d06d
sbaheti26 <shraddha.baheti26@gmail.com>
2020-12-18 18:31:16 -0800

    commiting

--- src/test/java/Demo_tekarch/QA_GIT/Git.java
+++ src/test/java/Demo_tekarch/QA_GIT/Git.java
@@ -8,9 +8,12 @@ public class Git {
 	/*
 	 * if u have to store ur projects or group of files.. using git.. advanced feature to
 	 * manage ur project. its a centralised repostiory ..
+	 * install git in local machine..
+	 * create one github account
 	 * inside machine...git+project----github
 	 * 1st--configure project to git --git init (command prompt)...this project initaize git
-	 * git repo to localmachine.....git remote add origin repourl(https://github.com/sbaheti26/demo_nov_2020.git)
+	 git status-- to check status
+	 * git repo to local git repo.....git remote add origin repourl(https://github.com/sbaheti26/demo_nov_2020.git)
 	 * to check it..git remote -v
 	 * git add readme.md
 	 * create .gitignore file from eclipse and  add unwanted files into it. 
@@ -23,6 +26,18 @@ public class Git {
 	 * working project to local git repo--git commit -m(some message)
 	 * 
 	 *  git config --global user.email "you@example.com"
+	 *  git push origin master...to push code to github..
+	 *  master is a branch name.
+	 *  deafult branch master.. u can also change it
+	 *  git branch <branchname>
+	 *  git branch
+	 *  how to delete branch name-- git branch -D <branchname>
+	 *  grren color name.. means that branch u r on that branch
+	 *  git checkout branchname-- to switch   to another branch
+	 *  help eclise-- eclipse marketplace-- search tab... git
+	 *  to display project name in eclipse.. right click on project.. team.. share project
+	 *  to see git staging view..windows..show view--others.. search git
+	 *  
 	 * 
 	 * 
 	 */
